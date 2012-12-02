3ccd18dc0138104ec25aa02731ff0b37eb81b77b
Deb G <dgangopa@gmail.com>
2012-12-02 17:48:45 -0500

    initial version

---
+++ app/models/apples.rb
@@ -0,0 +1,3 @@
+class Apples < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0c2a01a331f5e44336914cdf2bc0707db4461558cc2ff8728ae5c0df61ac8fbdedea308c253543a63e48fb65317218c8e25d6f812316047e45c909fe7c1d0f9b'
